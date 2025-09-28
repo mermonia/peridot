d7d3f6639446ba485b78742e514d94dcbb57ba12
Daniel Sanso <cs.daniel.sanso@gmail.com>
2025-09-28 17:54:39 +0200

    add global functions to ease logging from outside the module

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -3,9 +3,31 @@ package logger
 import (
 	"log/slog"
 	"os"
+
+	"github.com/fatih/color"
 )
 
 var defaultHandler = NewCustomHandler(os.Stdout,
 	&CustomOptions{Level: slog.LevelInfo, HidePC: true})
 
 var defaultLogger = slog.New(defaultHandler)
+
+func Debug(msg string, args ...any) {
+	defaultLogger.Debug(msg, args...)
+}
+
+func Info(msg string, args ...any) {
+	defaultLogger.Info(msg, args...)
+}
+
+func Warn(msg string, args ...any) {
+	color.Set(color.FgMagenta)
+	defaultLogger.Warn(msg, args...)
+	color.Unset()
+}
+
+func Error(msg string, args ...any) {
+	color.Set(color.FgRed)
+	defaultLogger.Error(msg, args...)
+	color.Unset()
+}
