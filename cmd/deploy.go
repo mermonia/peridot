a7d2bb6eb48dbd1d713f65e0b4834a8c457ec0b7
Daniel Sanso <cs.daniel.sanso@gmail.com>
2025-10-13 20:52:01 +0200

    add deploy command structure

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -2,16 +2,21 @@ package cmd
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/mermonia/peridot/internal/module"
+	"github.com/mermonia/peridot/internal/paths"
+	"github.com/mermonia/peridot/internal/state"
 	"github.com/urfave/cli/v3"
 )
 
 type DeployCommandConfig struct {
 	Simulate   bool
 	Overwrite  bool
+	Adopt      bool
 	Dotreplace bool
-	Dir        string
 	Root       string
+	ModuleName string
 }
 
 var deployCommandDescription string = `
@@ -51,25 +56,11 @@ var DeployCommand cli.Command = cli.Command{
 			Usage:   "don't make any changes, merely show what would be done",
 		},
 		&cli.BoolFlag{
-			Name:    "overwrite",
-			Aliases: []string{"O"},
-			Value:   false,
-			Usage: "if there are any files in the actual filesystem that share both name\n" +
-				"and path with a file in the module dir, overwite them",
-		},
-		&cli.BoolFlag{
 			Name:    "dotreplace",
 			Aliases: []string{"D"},
 			Value:   false,
 			Usage: "rename both the intermediate file and the symlink to the deployed\n" +
 				"files, from dot-* to .*",
-		},
-		&cli.StringFlag{
-			Name:      "dir",
-			Aliases:   []string{"d"},
-			Value:     "",
-			Usage:     "specify the path to the module dir to be deployed",
-			TakesFile: true,
 		},
 		&cli.StringFlag{
 			Name:    "root",
@@ -78,6 +69,31 @@ var DeployCommand cli.Command = cli.Command{
 			Usage: "specify the root path to which the module dir's structure should\n" +
 				"be deployed",
 			TakesFile: true,
+		},
+	},
+	MutuallyExclusiveFlags: []cli.MutuallyExclusiveFlags{
+		{
+			Required: false,
+			Flags: [][]cli.Flag{
+				{
+					&cli.BoolFlag{
+						Name:    "overwrite",
+						Aliases: []string{"O"},
+						Value:   false,
+						Usage: "forcefully replaces existing files in the filesystem by removing\n" +
+							"them and creating the symlink",
+					},
+				},
+				{
+					&cli.BoolFlag{
+						Name:    "adopt",
+						Aliases: []string{"a"},
+						Value:   false,
+						Usage: "Imports existing files by copying their contents into the module,\n" +
+							"then removes the originals and replaces them with symlinks",
+					},
+				},
+			},
 		},
 	},
 	Arguments: []cli.Argument{
@@ -90,9 +106,10 @@ var DeployCommand cli.Command = cli.Command{
 		cmdCfg := &DeployCommandConfig{
 			Simulate:   c.Bool("simulate"),
 			Overwrite:  c.Bool("overwrite"),
+			Adopt:      c.Bool("adopt"),
 			Dotreplace: c.Bool("dotreplace"),
-			Dir:        c.String("dir"),
 			Root:       c.String("root"),
+			ModuleName: c.StringArg("moduleName"),
 		}
 
 		return ExecuteDeploy(cmdCfg)
@@ -100,6 +117,52 @@ var DeployCommand cli.Command = cli.Command{
 }
 
 func ExecuteDeploy(cmdCfg *DeployCommandConfig) error {
+	dotfilesDir := paths.DotfilesDir()
+	moduleName := cmdCfg.ModuleName
+
+	st, err := state.LoadState(paths.StateFilePath())
+	if err != nil {
+		return fmt.Errorf("could not load state: %w")
+	}
+
+	moduleState := st.Modules[moduleName]
+	if moduleState == nil {
+		return fmt.Errorf("the specified module is not managed by peridot")
+	}
+
+	mod, err := module.Load(dotfilesDir, moduleName, moduleState)
+	if err != nil {
+		return fmt.Errorf("could not load module %s: %w", moduleName, err)
+	}
+
+	if !mod.ShouldDeploy(st) {
+		return fmt.Errorf("the module %s could not be deployed: %w", moduleName, err)
+	}
+
+	filesToDeploy := getFilesToDeploy(dotfilesDir, mod)
+	if cmdCfg.Simulate {
+		return simulateDeployment(dotfilesDir, mod, filesToDeploy, cmdCfg)
+	} else {
+		return deployFiles(dotfilesDir, mod, filesToDeploy, cmdCfg)
+	}
+}
+
+func getFilesToDeploy(dotfilesDir string, mod *module.Module) []string {
+	files := []string{}
+
+	// getting files logic
+
+	return files
+}
+
+func deployFiles(dotfilesDir string, mod *module.Module, files []string, cmdCfg *DeployCommandConfig) error {
+	//deployment logic
+
+	return nil
+}
+
+func simulateDeployment(dotfilesDir string, mod *module.Module, files []string, cmdCfg *DeployCommandConfig) error {
+	//simulation logic
 
 	return nil
 }
